71d30e468b1638b74bbe11a5df7b6b88cc94a4b5
velcronator <valacos7@gmail.com>
2024-08-05 16:15:12 +1000

    Enemy shooting

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -3,13 +3,27 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    [Header("Projectile")]
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float projectileLifetime = 5f;
-    [SerializeField] private float firingRate = 0.2f; // Time between shots
+    [Header("Firing Rate")]
+    [SerializeField] private float minFiringRate = 0.8f;
+    [SerializeField] private float maxFiringRate = 2f;
+    [SerializeField] private float baseFiringRate = 0.2f; // Time between shots
 
+    private bool isEnemy = false; 
     public bool isFiring = false;
     private Coroutine firingCoroutine;
+
+    private void Start()
+    {
+        if (gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            isEnemy = true;
+            isFiring = true;
+        }
+    }
 
     void Update()
     {
@@ -26,12 +40,25 @@ public class Shooter : MonoBehaviour
 
     private IEnumerator FireContinuously()
     {
+        if(isEnemy)
+        {   
+            moveSpeed = -moveSpeed;
+        }
         while (isFiring)
         {
             GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
             projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, moveSpeed);
             Destroy(projectile, projectileLifetime);
-            yield return new WaitForSeconds(firingRate); // Control the firing rate
+            yield return new WaitForSeconds(GetRandomFiringRate()); // Control the firing rate
         }
+    }
+
+    private float GetRandomFiringRate()
+    {
+        if(!isEnemy)
+        {
+            return baseFiringRate;
+        }
+        return Random.Range(minFiringRate, maxFiringRate);
     }
 }
